b831f9f47bd7dfa6d8698ec96300b68e84ffcedb
yangyang1 <yangyang02@bamboocloud.cn>
2025-10-22 10:06:27 +0800

    20251022-基础结构

--- src/main/java/com/lowcode/workflow/runner/graph/service/impl/NodeServiceImpl.java
+++ src/main/java/com/lowcode/workflow/runner/graph/service/impl/NodeServiceImpl.java
@@ -1,7 +1,8 @@
 package com.lowcode.workflow.runner.graph.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.lowcode.workflow.runner.graph.Node;
+
+import com.lowcode.workflow.runner.graph.data.struct.template.Node;
 import com.lowcode.workflow.runner.graph.mapper.NodeMapper;
 import com.lowcode.workflow.runner.graph.service.NodeService;
 import org.springframework.stereotype.Service;
